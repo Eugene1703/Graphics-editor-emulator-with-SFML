78ab98b92376ee2dc7a59a170ea760a613b799e6
Eugene1703 <91949945+Eugene1703@users.noreply.github.com>
2022-11-07 11:24:34 +0200

    Update test

--- LR1/Sample-Test1/Sample-Test1/test.cpp
+++ LR1/Sample-Test1/Sample-Test1/test.cpp
@@ -5,7 +5,7 @@ TEST(TestCmd, clear_1)
 {
   string str = "0 ff ff ff";
   my_parser.parse(const_cast<char*>(str.c_str()));
-  ASSERT_EQ(my_parser.my_answer.comm_id, 0);
+  ASSERT_EQ(my_parser.my_answer.comm_id, CLEAR_DISPLAY);
   ASSERT_TRUE(my_parser.my_answer.check);
   ASSERT_EQ(my_parser.my_answer.R, 255);
   ASSERT_EQ(my_parser.my_answer.G, 255);
@@ -15,7 +15,7 @@ TEST(TestCmd, clear_2)
 {
 	string str = "0 ffff ff";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 0);
+	ASSERT_EQ(my_parser.my_answer.comm_id, CLEAR_DISPLAY);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.R, 255);
 	ASSERT_EQ(my_parser.my_answer.G, 255);
@@ -25,7 +25,7 @@ TEST(TestCmd, draw_pixel)
 {
 	string str = "1 50 50 00 00 ff";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 1);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_PIXEL);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -37,7 +37,7 @@ TEST(TestCmd, draw_pixel_2)
 {
 	string str = "1 5050 00 00 ff";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 1);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_PIXEL);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -49,7 +49,7 @@ TEST(TestCmd, draw_line)
 {
 	string str = "2 50 50 50 50 ff 00 ff";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 2);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_LINE );
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -63,7 +63,7 @@ TEST(TestCmd, draw_line_2)
 {
 	string str = "2 50 50 5 50 50 ff 00 ff";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 2);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_LINE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -77,7 +77,7 @@ TEST(TestCmd, draw_rect)
 {
 	string str = "3 50 50 50 50 00 00 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 3);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_RECTANGLE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -91,7 +91,7 @@ TEST(TestCmd, draw_rect_2)
 {
 	string str = "3 50 50 50 00 00 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 3);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_RECTANGLE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -105,7 +105,7 @@ TEST(TestCmd, draw_fill)
 {
 	string str = "4 50 50 50 50 00 00 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 4);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_FILL_RECTANGLE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -119,7 +119,7 @@ TEST(TestCmd, draw_fill_2)
 {
 	string str = "4 50 50 50 50 00 00 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 4);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_FILL_RECTANGLE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -133,7 +133,7 @@ TEST(TestCmd, draw_ellipse)
 {
 	string str = "5 50 50 20 30 00 00 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 5);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_ELLIPSE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -147,7 +147,7 @@ TEST(TestCmd, draw_ellipse_2)
 {
 	string str = "5 50 50 20 30 fff 00 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 5);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_ELLIPSE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -161,7 +161,7 @@ TEST(TestCmd, draw_fill_ellipse)
 {
 	string str = "6 50 50 20 30 ff 00 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 6);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_FILL_ELLIPSE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -175,7 +175,7 @@ TEST(TestCmd, draw_fill_ellipse_2)
 {
 	string str = "6 50 50 20 30 ff 00 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 6);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_FILL_ELLIPSE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -189,7 +189,7 @@ TEST(TestCmd, draw_circle)
 {
 	string str = "7 50 50 20 00 ff 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 7);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_CIRCLE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -202,7 +202,7 @@ TEST(TestCmd, draw_circle_2)
 {
 	string str = "750 50 20 00 ff 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 7);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_CIRCLE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -215,7 +215,7 @@ TEST(TestCmd, draw_fill_circle)
 {
 	string str = "8 50 50 20 00 ff 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id,8);
+	ASSERT_EQ(my_parser.my_answer.comm_id,DRAW_FILL_CIRCLE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -228,7 +228,7 @@ TEST(TestCmd, draw_fill_circle_2)
 {
 	string str = "8 50 50 2000 ff 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 8);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_FILL_CIRCLE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -241,7 +241,7 @@ TEST(TestCmd, draw_round_rect)
 {
 	string str = "9 50 50 50 50 10 00 ff 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 9);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_ROUNDED_RECTANGLE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -256,7 +256,7 @@ TEST(TestCmd, draw_round_rect_2)
 {
 	string str = "A 50 5050 50 10 00 ff 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 10);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_ROUNDED_RECTANGLE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -271,7 +271,7 @@ TEST(TestCmd, draw_fill_round_rect)
 {
 	string str = "A 50 50 50 50 10 00 ff 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 10);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_FILL_ROUNDED_RECTANGLE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -286,7 +286,7 @@ TEST(TestCmd, draw_fill_round_rect_2)
 {
 	string str = "A50 50 50 50 10 00 ff 00";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 10);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_FILL_ROUNDED_RECTANGLE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 50);
 	ASSERT_EQ(my_parser.my_answer.y0, 50);
@@ -301,7 +301,7 @@ TEST(TestCmd, draw_text)
 {
 	string str = "B 100 100 ff 00 ff 16 5 hello";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 11);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_TEXT);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 100);
 	ASSERT_EQ(my_parser.my_answer.y0, 100);
@@ -316,7 +316,7 @@ TEST(TestCmd, draw_text_2)
 {
 	string str = "B 100 100 ff 00 ff 16 5hello";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 11);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_TEXT);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 100);
 	ASSERT_EQ(my_parser.my_answer.y0, 100);
@@ -331,7 +331,7 @@ TEST(TestCmd, draw_image)
 {
 	string str = "C 100 100 200 200";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 12);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_IMAGE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 100);
 	ASSERT_EQ(my_parser.my_answer.y0, 100);
@@ -342,7 +342,7 @@ TEST(TestCmd, draw_image_2)
 {
 	string str = "C 100 100 200200";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 12);
+	ASSERT_EQ(my_parser.my_answer.comm_id, DRAW_IMAGE);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.x0, 100);
 	ASSERT_EQ(my_parser.my_answer.y0, 100);
@@ -353,7 +353,7 @@ TEST(TestCmd, set_orient)
 {
 	string str = "D 2";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 13);
+	ASSERT_EQ(my_parser.my_answer.comm_id, SET_ORIENTATION);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.orientation, 2);
 }
@@ -361,7 +361,7 @@ TEST(TestCmd, set_orient_2)
 {
 	string str = "D 200";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 13);
+	ASSERT_EQ(my_parser.my_answer.comm_id, SET_ORIENTATION);
 	ASSERT_TRUE(my_parser.my_answer.check);
 	ASSERT_EQ(my_parser.my_answer.orientation, 2);
 }
@@ -369,7 +369,7 @@ TEST(TestCmd, Get_Width)
 {
 	string str = "E";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 14);
+	ASSERT_EQ(my_parser.my_answer.comm_id, GET_WIDTH);
 	ASSERT_EQ(my_parser.my_answer.width, 1280);
 	ASSERT_TRUE(my_parser.my_answer.check);
 }
@@ -377,7 +377,7 @@ TEST(TestCmd, Get_Width_2)
 {
 	string str = "Eee";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 14);
+	ASSERT_EQ(my_parser.my_answer.comm_id, GET_WIDTH);
 	ASSERT_EQ(my_parser.my_answer.width, 1280);
 	ASSERT_TRUE(my_parser.my_answer.check);
 }
@@ -385,14 +385,14 @@ TEST(TestCmd, Get_Height)
 {
 	string str = "F";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 15);
+	ASSERT_EQ(my_parser.my_answer.comm_id, GET_HEIGHT);
 	ASSERT_EQ(my_parser.my_answer.height, 720);
 }
 TEST(TestCmd, Get_Height_2)
 {
 	string str = "Fffff";
 	my_parser.parse(const_cast<char*>(str.c_str()));
-	ASSERT_EQ(my_parser.my_answer.comm_id, 15);
+	ASSERT_EQ(my_parser.my_answer.comm_id, GET_HEIGHT);
 	ASSERT_EQ(my_parser.my_answer.height, 720);
 }
 int main(int argc, char** argv)
